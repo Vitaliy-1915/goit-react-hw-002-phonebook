ed81160071db6d4b67d08c1bed8f6ea2ac942508
Vitaliy-1915 <74369854+Vitaliy-1915@users.noreply.github.com>
2021-11-10 02:05:49 +0200

    вычисления

--- src/Components/PhoneBookCard/PhoneBookCard.jsx
+++ src/Components/PhoneBookCard/PhoneBookCard.jsx
@@ -1,28 +1,73 @@
 import React, { Component } from 'react';
 import PhoneBookCardTitle from '../PhoneBookCardTitle/PhoneBookCardTitle';
+import ContactsList from '../ContactsList/ContactsList';
+import Form from '../Form/Form';
+import Filter from '../Filter/Filter';
+import { v4 as unId } from 'uuid';
 
 export class PhoneBookCard extends Component {
   state = {
-    contacts: [],
-    name: '',
+    contacts: [
+      { id: 'id-1', name: 'Rosie Simpson', number: '459-12-56' },
+      { id: 'id-2', name: 'Hermione Kline', number: '443-89-12' },
+      { id: 'id-3', name: 'Eden Clements', number: '645-17-79' },
+      { id: 'id-4', name: 'Annie Copeland', number: '227-91-26' },
+    ],
+    filter: '',
+  };
+
+  formSubmit = value => {
+    const contact = {
+      id: unId(),
+      name: value.name,
+      number: value.number,
+    };
+
+    //   this.setState((prevState) => ({
+    // contacts: [...prevState.contacts, contact],
+    //   }));
+
+    this.state.contacts.find(contact =>
+      contact.name.toLowerCase().includes(contact.name.toLowerCase()),
+    )
+      ? alert(`${contact.name} is already in contacts`)
+      : this.setState(prevState => ({
+          contacts: [...prevState.contacts, contact],
+        }));
+  };
+
+  onChangeFilter = e => {
+    this.setState({
+      filter: e.currentTarget.value,
+    });
+  };
+
+  onDeleteContact = contactId => {
+    // console.log(contactId);
+    this.setState(prevState => ({
+      contacts: prevState.contacts.filter(contact => contact.id !== contactId),
+    }));
   };
 
   render() {
+    // const { contacts, filter } = this.state;
+
+    const normalizeContacts = this.state.filter.toLocaleLowerCase();
+
+    const filterContacts = this.state.contacts.filter(contact =>
+      contact.name.toLowerCase().includes(normalizeContacts),
+    );
+
     return (
       <div>
         <PhoneBookCardTitle title="Phonebook" />
-        <form>
-          <label>
-            <input
-              type="text"
-              name="name"
-              pattern="^[a-zA-Zа-яА-Я]+(([' -][a-zA-Zа-яА-Я ])?[a-zA-Zа-яА-Я]*)*$"
-              title="Имя может состоять только из букв, апострофа, тире и пробелов. Например Adrian, Jacob Mercer, Charles de Batz de Castelmore d'Artagnan и т. п."
-              required
-            />
-          </label>
-        </form>
+        <Form onSubmit={this.formSubmit} />
         <PhoneBookCardTitle title="Contacts" />
+        <Filter value={this.state.filter} onChange={this.onChangeFilter} />
+        <ContactsList
+          contacts={filterContacts}
+          onDeleteContact={this.onDeleteContact}
+        />
       </div>
     );
   }
